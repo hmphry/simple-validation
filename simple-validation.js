da74b6fa32a43f5741acc41f0c0db7965d612595
drmyersii <drmyersii@users.noreply.github.com>
2014-07-15 23:08:16 -0400

    A little more code refactoring...
    I know it seems like overkill, but now the code is factored out into a
    namespace pretty well. This will allow for a huge opportunity of
    customization in the future without packing extra features in this
    library. This will keep it light but also allow for devs to pack in and
    use more features such as the validation helper methods. There are two
    main calls to make now: SimpleValidator.Initialize() which sets all event
    handlers including the form submit event handler, and also
    SimpleValidator.Validate() which forces a global validation at the moment.
    That means all forms and all formElements that aren't set to novalidate
    will validate immediately. If you want to validate just one form randomly
    or even just validate a subset of forms, call the helper method
    SimpleValidator.Helpers.ValidateForm() or
    SimpleValidator.Helpers.ValidateForms().

--- simple-validation.js
+++ simple-validation.js
@@ -24,6 +24,51 @@
 		},
 
 		Helpers: {
+			GetForms: function ()
+			{
+				var forms = $(SimpleValidator.Config.Selectors.Forms).not(SimpleValidator.Config.Selectors.FormsToIgnore);
+
+				return forms;
+			},
+
+			GetFormElements: function (form)
+			{
+				var formElements = form.find(SimpleValidator.Config.Selectors.FormElements).not(SimpleValidator.Config.Selectors.FormElementsToIgnore); // This will find all the whitelisted form elements to validate and ignore the ones marked as "novalidate"
+
+				return formElements;
+			},
+
+			InitializeForm: function (form)
+			{
+				var formElements = SimpleValidator.Helpers.GetFormElements(form);
+
+				formElements.on('blur', function ()
+				{
+					var formElement = $(this);
+
+					SimpleValidator.Helpers.ValidateFormElement(formElement);
+				});
+
+				form.on('submit', function (e) // Only set the handler on forms that need to be validated.
+				{
+					e.preventDefault(); // Prevents the default submission of the form. This lets us validate before actually submitting the form.
+					SimpleValidator.Helpers.ValidateFormElements(formElements); // Do a final validation on formElements since validation is only called on leaving an formElement and the user may not have left the last formElement.
+
+					if (!formElements.is('.invalid')) // If any formElements are marked as invalid, don't submit the form.
+						form.unbind('submit').trigger('submit'); // Unbind this form submit event and then resubmit since the form is valid.
+				});
+			},
+
+			InitializeForms: function (forms)
+			{
+				forms.each(function ()
+				{
+					var form = $(this);
+
+					SimpleValidator.Helpers.InitializeForm(form);
+				});
+			},
+
 			MarkAsInvalid: function (formElement)
 			{
 				formElement.removeClass('valid');
@@ -34,6 +79,23 @@
 			{
 				formElement.removeClass('invalid');
 				formElement.addClass('valid');
+			},
+
+			ValidateForm: function (form)
+			{
+				var formElements = SimpleValidator.Helpers.GetFormElements(form);
+
+				SimpleValidator.Helpers.ValidateFormElements(formElements);
+			},
+
+			ValidateForms: function (forms)
+			{
+				forms.each(function ()
+				{
+					var form = $(this);
+
+					SimpleValidator.Helpers.ValidateForm(form);
+				});
 			},
 
 			ValidateFormElement: function (formElement) // Validates a single formElement
@@ -92,37 +154,22 @@
 			}
 		},
 
-		Validate: function () // Initializes event handlers to start validating forms
+		Initialize: function () // Initializes event handlers to start validating forms
 		{
-			// By default, it will not validate forms with the class "novalidate" or the "novalidate" attribute. If you want to disable html5 validation and still use this class, 
-			// just remove the [novalidate] attribute from the following line like so: var forms = $('form').not('.novalidate');
-			var forms = $(SimpleValidator.Config.Selectors.Forms).not(SimpleValidator.Config.Selectors.FormsToIgnore);
+			var forms = SimpleValidator.Helpers.GetForms();
 
-			forms.each(function () // Set the event handlers for each form individually. I may change this to set all handlers at once in the future...
-			{
-				var form = $(this);
-				var formElements = form.find(SimpleValidator.Config.Selectors.FormElements).not(SimpleValidator.Config.Selectors.FormElementsToIgnore); // This will find all the whitelisted form elements to validate and ignore the ones marked as "novalidate"
+			SimpleValidator.Helpers.InitializeForms(forms);
+		},
 
-				formElements.on('blur', function ()
-				{
-					var formElement = $(this);
+		Validate: function () // Forces global validation of forms.
+		{
+			var forms = SimpleValidator.Helpers.GetForms();
 
-					SimpleValidator.Helpers.ValidateFormElement(formElement);
-				});
-
-				form.on('submit', function (e) // Only set the handler on forms that need to be validated.
-				{
-					e.preventDefault(); // Prevents the default submission of the form. This lets us validate before actually submitting the form.
-					SimpleValidator.Helpers.ValidateFormElements(formElements); // Do a final validation on formElements since validation is only called on leaving an formElement and the user may not have left the last formElement.
-
-					if (!formElements.is('.invalid')) // If any formElements are marked as invalid, don't submit the form.
-						form.unbind('submit').trigger('submit'); // Unbind this form submit event and then resubmit since the form is valid.
-				});
-			});
+			SimpleValidator.Helpers.ValidateForms(forms);
 		}
 	}
 
 	window.SimpleValidator = SimpleValidator;
 })(window);
 
-window.SimpleValidator.Validate();
+window.SimpleValidator.Initialize(); // This call will initialize all event handlers. 
